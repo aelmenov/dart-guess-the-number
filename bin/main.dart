ff575732ce15bdc660a2637cf80d2e69937c716a
Alex Elmenov <alex.elmenov@gmail.com>
2019-05-26 03:58:06 +0300

    Fix on maximal random number

--- bin/main.dart
+++ bin/main.dart
@@ -4,7 +4,7 @@ import 'dart:math';
 import '../lib/compare-input.dart';
 
 void main() {
-  int random = new Random.secure().nextInt(2);
+  int random = new Random.secure().nextInt(20);
 
   print("Welcome to the \"Guess The Number\" game!\nRules are simple — you have to guess a number between 0 to 20.\n");
 
